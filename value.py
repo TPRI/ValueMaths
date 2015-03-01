129bc0224544741069813c43da0d3fff349b3077
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2015-03-01 11:09:28 +0000

    Value class to implement a quantity with a 'number' and 'unit'

---
+++ value.py
@@ -0,0 +1,18 @@
+# A class to implement a value/quantity that consists of a number and a unit
+
+class Value(object):
+
+    # Constructor
+    def __init__(self, *args):
+        pass
+
+        # reads different data types
+
+    # Multiply
+
+    # Add (Check unit compatibility)
+
+    # Equality
+
+    # __str__
+
