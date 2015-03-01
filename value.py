c853a3c8873351073050f9d8624ff26de4facb13
Tim Rose-Innes <timothy.rose-innes.14@ucl.ac.uk>
2015-03-01 15:53:55 +0000

    Refactored out the self.data middleman in the __init__ method

--- value.py
+++ value.py
@@ -14,7 +14,7 @@ class Value(object):
         # Check the type of lead
         if type(lead) == type(self):
             # Copy constructor
-            self.data = dict(lead.data)  # TODO check behavior
+            self.base_units = lead.base_units
             self.coefficient = lead.coefficient
         elif type(lead) == int:
             self.from_constant(lead)
@@ -25,9 +25,7 @@ class Value(object):
         else:
             self.from_lists(*args)
 
-        self.to_base_units()
-
-    def to_base_units(self):
+    def to_base_units(self, data):
         # load the unit definitions
         units = yaml.load(open(os.path.join(os.path.dirname(__file__), 'unit_defs.yml')))
 
@@ -35,7 +33,7 @@ class Value(object):
         self.base_units = {'m': 0, 'kg': 0, 's': 0, 'A': 0, 'k': 0, 'cd': 0, 'mol': 0}
 
         # Convert symbol,exponent in data into base units
-        for symbol, exponent in self.data.iteritems():
+        for symbol, exponent in data.iteritems():
             new_unit = units[symbol]
             new_base = new_unit['base']
             new_num = new_unit['num']
@@ -48,51 +46,30 @@ class Value(object):
                 self.base_units[key] += new_base[key]*exponent
 
 
-    # lead int -> constant
+    # lead int 
     def from_constant(self, constant):
         self.coefficient = constant
-        self.data = {}
+        self.to_base_units({})
 
-    # lead str -> symbol
+    # lead str
     def from_symbol(self, symbol, coefficient=1, power=1):
         self.coefficient = coefficient
-        self.data = {symbol: power}
+        self.to_base_units({symbol: power})
 
-    # lead dict -> data
-    def from_dictionary(self, data, coefficient=1):
-        self.data = data
+    # lead dict
+    def from_dictionary(self, dictionary, coefficient=1):
         self.coefficient = coefficient
+        self.to_base_units(dictionary)
 
-    # lead list -> data
+    # lead list
     def from_lists(self, symbols=[], powers=[], coefficient=1):
         self.coefficient = coefficient
-        self.data = {symbol: exponent for symbol, exponent
-                     in zip(symbols, powers)}
+        self.to_base_units({symbol: exponent for symbol, exponent
+                     in zip(symbols, powers)})
 
     # Multiply
     def multiply(self, *others):
-
-        result_data = dict(self.data)
-        result_coeff = self.coefficient
-
-        # Convert arguments to Terms first if they are
-        # constants or integers
-        others = map(Value, others)
-        for another in others:
-            for symbol, exponent in another.data.iteritems():
-                if symbol in result_data:
-                    result_data[symbol] += another.data[symbol]
-                else:
-                    result_data[symbol] = another.data[symbol]
-            result_coeff *= another.coefficient
-        return Value(result_data, result_coeff)
-
-
-    def __mul__(self, other):
-        return self.multiply(other)
-
-    def __rmul__(self, other):
-        return self.__mul__(other)
+        pass
 
     # Add (Check unit compatibility)
     def add(self, other):
